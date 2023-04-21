042edd12f6fa35753680c48e411c48be89a8e399
Colby Fayock <fayock@gmail.com>
2023-04-20 22:10:54 -0400

    01 - Installing & Configuring the OpenAI SDK

--- src/pages/api/image.js
+++ src/pages/api/image.js
@@ -1,3 +1,11 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { Configuration, OpenAIApi } from 'openai';
+const configuration = new Configuration({
+    organization: process.env.OPENAI_ORGANIZATION,
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req, res) {
+  const response = await openai.listEngines();
+  res.status(200).json({ data: response.data.data })
 }
