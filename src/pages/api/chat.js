7891fdb139469a04d0a073bc29354645bddde20d
Colby Fayock <fayock@gmail.com>
2023-04-20 22:17:45 -0400

    04 - Chat Completion with GPT

---
+++ src/pages/api/chat.js
@@ -0,0 +1,23 @@
+import { Configuration, OpenAIApi } from 'openai';
+const configuration = new Configuration({
+    organization: process.env.OPENAI_ORGANIZATION,
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req, res) {
+
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content: 'what is the first online game to be played competitively?'
+      }
+    ]
+  });
+
+  res.status(200).json({
+    data: completion.data
+  })
+}
